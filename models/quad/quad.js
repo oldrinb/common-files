0866578f776edb4fc37b51a7a158e818535c2cab
Oldrin Barbulescu <57703366+oldrinb@users.noreply.github.com>
2024-09-12 18:51:17 -0700

    Add files via upload

---
+++ models/quad/quad.js
@@ -0,0 +1,28 @@
+/*<?xml version="1.0" encoding="utf-8"?>*/
+
+/**
+ * Author: Oldrin Bărbulescu
+ * Last modified: Aug 2, 2024
+ **/
+
+if (typeof models !== "object")  models = {};
+
+models.quad = { 
+  positions : [
+    -1.0, -1.0, 0.0,
+     1.0, -1.0, 0.0,
+    -1.0,  1.0, 0.0,
+    -1.0,  1.0, 0.0,
+     1.0, -1.0, 0.0,
+     1.0,  1.0, 0.0
+  ],
+
+  texCoords : [
+    0.0, 0.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    0.0, 1.0,
+    1.0, 0.0,
+    1.0, 1.0
+  ]
+};
